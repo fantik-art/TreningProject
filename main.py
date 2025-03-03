b2a4dd01c9eb41823fe3b782a8cc74f0c096cf09
fantik-art <artemis.shulga022@gmail.com>
2025-03-03 18:55:42 +0300

    new function

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ print('hello GIT!')
 
 name = input('What your name? ')
 
-print(f'My name is {name}')
-
-
-print(f'this wery cool!')
-
 print('trening Brunch')
+
+f = 'Cool' if name=='artem' else 'back'
