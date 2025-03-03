d81d35f89c77228d60217c3e3ba0ce1741e6dfc3
fantik-art <artemis.shulga022@gmail.com>
2025-03-03 19:28:31 +0300

    new func filter

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ name = input('What your name? ')
 print(f'My name is {name}')
 
 print(f'this wery cool!')
+
+def main(name):
+    new = list(filter(lambda x: x =='a', name))
+    return new
+
+xxx = main(['a', 'k'])
+print(xxx)
