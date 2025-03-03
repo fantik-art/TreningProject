80808a704ed160d9e21dccb4ea330a4d2f66c8b3
fantik-art <artemis.shulga022@gmail.com>
2025-03-03 19:17:52 +0300

    new list function

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ name = input('What your name? ')
 print('trening Brunch')
 
 f = 'Cool' if name=='artem' else 'back'
+print(f)
+xx = ['qwe','sdasda']
+z = list(map(lambda x: x.upper(), xx))
+
+print(z)
